f624d5eab567eed3e46374931861a702af8cff63
Thiago Kasper de Souza <164453378+ThiagoKasperSouza@users.noreply.github.com>
2026-09-04 09:32:17 -0300

    Update pm.hs

--- src/pm.hs
+++ src/pm.hs
@@ -1,15 +1,25 @@
 -- Soma os 3 valores e divide pela quantidade
-calculaMedia :: Float -> Float -> Float -> Float
-calculaMedia a b c = (a+b+c)/3
+calculateAverage :: Float -> Float -> Float -> Float
+calculateAverage a b c = (a+b+c)/3
 
 -- Checa com base nas notas de corte
-passouNaMateria :: Bool -> String
+hasPassed :: Bool -> String
 
-passouNaMateria True = "Aprovado"
-passouNaMateria False = "Recuperação"
+hasPassed True = "Approved"
+hasPassed False = "Exam"
+
+coffeeType :: String -> String
+
+coffeeType "Expresso" = "Making coffee 1"
+coffeeType "Capuccino" = "Making coffee 2"
+coffee _ = "Invalid coffee type"
 
 main:: IO()
 main = do
-    let notaCorte = 6.0
-    putStrLn (passouNaMateria ((calculaMedia 7.0 5.0  6.5) >=notaCorte))
-    putStrLn (passouNaMateria ((calculaMedia 5.0 2.0 3.0) >=notaCorte))
+    let averageForApproval = 6.0
+    putStrLn (hasPassed ((calculateAverage 7.0 5.0  6.5) >=averageForApproval))
+    putStrLn (hasPassed ((calculateAverage 5.0 2.0 3.0) >=averageForApproval))
+
+    putStrLn coffeeType "Expresso"
+    putStrLn coffeeType "Capuccino"
+    putStrLn coffeeType "Bread"
